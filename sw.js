a0453c521e25315467e4797edc6f4be2fe6f72b3
Simon Svahn <simon.svahn@ri.se>
2026-07-25 14:05:19 +0200

    Publicering 2026-07-25

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Sidan: nätverk först (nya publiceringar slår igenom direkt), cache som
 // offline-reserv. Ikoner/manifest: cache först. Payloaden cachar appen själv i IndexedDB.
-const CACHE='kthsangbok-skal-bb8ba3f44441';
+const CACHE='kthsangbok-skal-c17948a88d82';
 const FILER=['.','index.html','manifest.json','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILER)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(
